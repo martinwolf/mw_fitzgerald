fe04b82fcfe302f56a18dfca645ae4e3cb5631c5
Martin Wolf <martin@martinwolf.org>
2015-07-07 14:30:17 +0200

    Init header.php, footer.php and load in index.php

---
+++ header.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+    <meta charset="<?php bloginfo( 'charset' ); ?>">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+    <!--
+        <link rel="dns-prefetch" href="//www.google-analytics.com">
+    -->
+
+    <?php wp_head(); ?>
+</head>
+
+<body <?php body_class(); ?>>

b76ca8d2791f7d4fc96ff01efad1586b5f25dd57
Martin Wolf <martin@martinwolf.org>
2015-07-08 12:05:56 +0200

    Add some basic article HTML

---
+++ content.php
@@ -0,0 +1,29 @@
+<article id="post-<?php the_ID(); ?>" <?php post_class('c-article'); ?>>
+    <header class="c-article__header">
+        <p class="c-article__category">
+            <?php
+            echo sprintf(
+                __( 'Published in  %s' ),
+                get_the_category_list( __( ', ' ) )
+            );
+            ?>
+        </p>
+
+        <?php
+        if ( is_single() ) :
+            the_title( '<h1 class="c-article__title">', '</h1>' );
+        else :
+            the_title( sprintf( '<h2 class="c-article__title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' );
+        endif;
+        ?>
+    </header>
+
+    <div class="c-txt">
+        <?php
+            the_content( sprintf(
+                __( 'Continue reading %s' ),
+                the_title()
+            ) );
+        ?>
+    </div>
+</article>

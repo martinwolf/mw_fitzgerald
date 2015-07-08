043120d5e8d12b5889335b93631406c048819992
Martin Wolf <martin@martinwolf.org>
2015-07-08 12:05:13 +0200

    Load Google fonts with webfontloader

--- header.php
+++ header.php
@@ -10,7 +10,26 @@
         <link rel="dns-prefetch" href="//www.google-analytics.com">
     -->
 
+    <script>
+        var WebFontConfig = {
+            google: {
+                families: [ 'Merriweather:400,700,400italic', 'Raleway:700' ]
+            },
+            timeout: 2000
+        };
+
+        (function(d) {
+            var wf = d.createElement('script'), s = d.scripts[0];;
+            wf.src = 'https://ajax.googleapis.com/ajax/libs/webfont/1.5.18/webfont.js';
+            wf.async = 'true';
+            s.parentNode.insertBefore(wf, s);
+        })(document);
+    </script>
+
     <?php wp_head(); ?>
 </head>
 
 <body <?php body_class(); ?>>
+
+<header class="c-site-header">
+</header>

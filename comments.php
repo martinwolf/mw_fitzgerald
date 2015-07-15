1d621c3488d793afcc3f01c258ba75e11b69aa19
Martin Wolf <martin@martinwolf.org>
2015-07-15 13:47:13 +0200

    Comments Template

---
+++ comments.php
@@ -0,0 +1,40 @@
+<?php
+/*
+ * If the current post is protected by a password and
+ * the visitor has not yet entered the password we will
+ * return early without loading the comments.
+ */
+if ( post_password_required() ) {
+    return;
+}
+?>
+
+<div id="comments" class="o-wrapper c-comments">
+
+    <?php if ( have_comments() ) : ?>
+        <h2 class="c-comments__title">
+            <?php comments_number('No Comments', 'One Comment', '% Comments' );?>
+        </h2>
+
+        <ol class="c-comments__list">
+            <?php
+                wp_list_comments( array(
+                    'style'       => 'ol',
+                    'short_ping'  => true,
+                    'avatar_size' => 0,
+                ) );
+            ?>
+        </ol>
+
+    <?php endif; ?>
+
+    <?php
+        // If comments are closed and there are comments, let's leave a little note, shall we?
+        if ( ! comments_open() && get_comments_number() && post_type_supports( get_post_type(), 'comments' ) ) :
+    ?>
+        <p class="no-comments"><?php _e( 'Comments are closed.' ); ?></p>
+    <?php endif; ?>
+
+    <?php comment_form(); ?>
+
+</div>

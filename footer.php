ee48991c389cbf3306249c648d421a29e71b065c
Martin Wolf <martin@martinwolf.org>
2015-07-15 13:47:26 +0200

    Categories and Pages in Footer

--- footer.php
+++ footer.php
@@ -1,4 +1,24 @@
-<footer class="c-site-footer">
+<footer class="o-wrapper c-site-footer">
+    <div class="o-grid">
+        <div class="o-grid__item w-1-2--m w-1-2--l">
+            <div class="c-site-footer__module">
+                <h3 class="c-site-footer__headline">Pages</h3>
+                <ul class="c-site-footer__list">
+                    <?php wp_list_pages('title_li='); ?>
+                </ul>
+            </div>
+        </div><!--
+
+     --><div class="o-grid__item w-1-2--m w-1-2--l">
+            <div class="c-site-footer__module">
+                <h3 class="c-site-footer__headline">Categories</h3> <ul class="c-site-footer__list">
+                    <?php wp_list_categories('title_li='); ?>
+                </ul>
+            </div>
+        </div>
+    </div>
+
+    <p class="c-site-footer__module">&copy; <?php bloginfo('author'); ?></p>
 </footer>
 
 <?php wp_footer(); ?>

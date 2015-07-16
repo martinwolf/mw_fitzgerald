26584dd8b3f298978de479611ce3ac4cf692c233
Martin Wolf <martin@martinwolf.org>
2015-07-16 11:34:43 +0200

    Add pagination

--- index.php
+++ index.php
@@ -1,19 +1,23 @@
 <?php get_header(); ?>
 
 <main>
-<?php
-if ( have_posts() ) :
+    <?php
+    if ( have_posts() ) :
 
-    while ( have_posts() ) : the_post();
-        get_template_part( 'content', get_post_format() );
-    endwhile;
+        while ( have_posts() ) : the_post();
+            get_template_part( 'content', get_post_format() );
+        endwhile;
 
-else:
+    else:
 
-    get_template_part( 'content', 'none' );
+        get_template_part( 'content', 'none' );
 
-endif;
-?>
+    endif;
+    ?>
 </main>
+
+<div class="o-wrapper c-pagination">
+    <?php posts_nav_link('&nbsp; | &nbsp;','Previous Page','Next Page'); ?>
+</div>
 
 <?php get_footer(); ?>

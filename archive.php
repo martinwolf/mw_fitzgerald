af41b5190f3998835ad4c699fb49c1623db81c74
Martin Wolf <martin@martinwolf.org>
2015-07-20 13:56:18 +0200

    Add Archive template and styles

--- archive.php
+++ archive.php
@@ -4,6 +4,8 @@
 <?php
 if ( have_posts() ) :
 
+    the_archive_title( '<h1 class="o-wrapper c-archive__title">', '</h1>' );
+
     while ( have_posts() ) : the_post();
         get_template_part( 'content', get_post_format() );
     endwhile;

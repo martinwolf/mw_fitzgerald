51c1b428f567d90e34eecf4feaf8e354f681ee45
Martin Wolf <martin@martinwolf.org>
2015-07-20 11:46:45 +0200

    Add 404 page and fix overflow of pre tag

---
+++ 404.php
@@ -0,0 +1,13 @@
+<?php get_header(); ?>
+
+<article class="c-article">
+    <header class="o-wrapper">
+        <h1 class="c-article__title">Error 404 - Page not found</h1>
+    </header>
+
+    <div class="o-wrapper c-text">
+        <p>It looks like nothing was found at this location. Maybe go back to the <a href="<?php bloginfo('home'); ?>">homepage</a>?</p>
+    </div>
+</article>
+
+<?php get_footer(); ?>
